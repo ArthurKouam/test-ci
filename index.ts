9307385e32da36ec89ab1343d2387b8fcf324403
ArthurKouam <gkouamarthur@gmail.com>
2025-09-21 10:34:17 +0100

    frist test

---
+++ index.ts
@@ -0,0 +1,19 @@
+import { createServer } from "node:http";
+import isOdd from 'is-odd'; 
+
+
+const server = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+
+  const num = req.url ? parseInt(req.url.slice(1)) : NaN;
+  if (!isNaN(num)) {
+    res.write(`Is ${num} odd? ${isOdd(num)}\n`);
+  }
+
+  res.end("Hello, World!");
+});
+
+server.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
